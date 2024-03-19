5b9e6f0a857c3e7f9a070979af92a218627c3a16
Vorapol (PCH) <vorapolst@gmail.com>
2024-03-19 16:54:36 +0700

    Add Sample Result

--- pandas/1-pandas-simple.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("../data/SuperStoreSimple.csv")
-df['Cost'] = df['Sales'] - df['Profit']
-
-print(df)
--- pandas/3-pandas-datareader.py
+++
@@ -1,9 +0,0 @@
-import pandas_datareader.data as pdr
-import yfinance as yf
-yf.pdr_override()
-
-df = pdr.get_data_yahoo('AAPL', start='2023-01-01', end='2024-03-15').reset_index()
-df['Ticker'] = 'AAPL'
-df = df[['Ticker','Date','Close']]
-
-print(df)
